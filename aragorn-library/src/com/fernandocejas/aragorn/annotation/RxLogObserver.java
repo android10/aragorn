ef5fee09eb0cff32061041a84f3a3968b6452779
Fernando Cejas <fcejas@gmail.com>
2015-09-15 23:26:21 +0200

    Refactor gradle build files and add tasks for sample execution.

---
+++ aragorn-library/src/com/fernandocejas/aragorn/annotation/RxLogObserver.java
@@ -0,0 +1,12 @@
+package com.fernandocejas.aragorn.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target({TYPE})
+public @interface RxLogObserver {
+}
