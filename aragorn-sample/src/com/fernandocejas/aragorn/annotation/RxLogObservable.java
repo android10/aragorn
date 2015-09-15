81350ac9159e0fb538c501ed61fa68b2570915bf
Fernando Cejas <fcejas@gmail.com>
2015-09-15 21:54:25 +0200

    Added annotations

---
+++ aragorn-sample/src/com/fernandocejas/aragorn/annotation/RxLogObservable.java
@@ -0,0 +1,12 @@
+package com.fernandocejas.aragorn.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target({METHOD})
+public @interface RxLogObservable {
+}
