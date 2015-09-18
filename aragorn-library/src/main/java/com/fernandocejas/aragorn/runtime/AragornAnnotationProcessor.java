af2d606414ef3e330e080e8c1a76414ac114e45f
Fernando Cejas <fcejas@gmail.com>
2015-09-18 10:17:35 +0200

    Add annotation processor class.

---
+++ aragorn-library/src/main/java/com/fernandocejas/aragorn/runtime/AragornAnnotationProcessor.java
@@ -0,0 +1,13 @@
+package com.fernandocejas.aragorn.runtime;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.TypeElement;
+import java.util.Set;
+
+public class AragornAnnotationProcessor extends AbstractProcessor {
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        return false;
+    }
+}
