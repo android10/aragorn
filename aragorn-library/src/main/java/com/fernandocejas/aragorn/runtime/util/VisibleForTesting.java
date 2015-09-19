ad0a90dc25259dd3b3772ff8488e1ef83227d6ee
Fernando Cejas <fcejas@gmail.com>
2015-09-19 18:07:03 +0200

    Add VisibleForTesting annotation.

---
+++ aragorn-library/src/main/java/com/fernandocejas/aragorn/runtime/util/VisibleForTesting.java
@@ -0,0 +1,8 @@
+package com.fernandocejas.aragorn.runtime.util;
+
+/**
+ * Indicates that the visibility of a type or member has been relaxed to make the code testable.
+ * Idea borrowed from Google Collections :)
+ */
+public @interface VisibleForTesting {
+}
