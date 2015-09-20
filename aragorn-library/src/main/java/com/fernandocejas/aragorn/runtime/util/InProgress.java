0fac6de13de55d47307da63384f4986bfede4720
Fernando Cejas <fcejas@gmail.com>
2015-09-20 20:46:15 +0200

    Add InProgress annotation

---
+++ aragorn-library/src/main/java/com/fernandocejas/aragorn/runtime/util/InProgress.java
@@ -0,0 +1,9 @@
+package com.fernandocejas.aragorn.runtime.util;
+
+/**
+ * Used to indicates that the annotated member is in Progress.
+ * A string value can be used for more information, such as a url or a description.
+ */
+public @interface InProgress {
+    String value() default "";
+}
