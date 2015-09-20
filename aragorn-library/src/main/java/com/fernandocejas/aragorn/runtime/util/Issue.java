d544ae764441c6d34d6a5ad07632547f5d11149b
Fernando Cejas <fcejas@gmail.com>
2015-09-20 20:46:24 +0200

    Add issue annotation.

---
+++ aragorn-library/src/main/java/com/fernandocejas/aragorn/runtime/util/Issue.java
@@ -0,0 +1,9 @@
+package com.fernandocejas.aragorn.runtime.util;
+
+/**
+ * Used to indicates that the annotated member has a relation with an issue.
+ * A string value can be used for more information, such as a url or a description.
+ */
+public @interface Issue {
+    String value() default "";
+}
